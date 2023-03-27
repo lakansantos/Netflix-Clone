1da2c26f8e4bbba17f4a84eedc11e6f67c12d1e5
ITW <rizesuoracle25@gmail.com>
2023-03-27 08:17:35 +0800

    added banner contents

--- src/views/movies/MoviesBanner.jsx
+++ src/views/movies/MoviesBanner.jsx
@@ -2,13 +2,24 @@ import Navbar from "./MoviesNavbar";
 import {BsFillPlayFill} from 'react-icons/bs'
 import {AiOutlineInfoCircle} from 'react-icons/ai'
 import {AiOutlineReload} from 'react-icons/ai'
+import movieTitle  from '/src/assets/images/movie-title.png'
+import netflixLogo from '/src/assets/images/netflix-logo.png'
 const Banner = () => {
     return (
         <div className="banner">
             <Navbar />
             <div className="movie-contents">
-                <h1 className="banner-movie-title">Lucifer</h1>
-                <p className="banner-movie-description">Bored with being the Lord of Hell, the devil relocates to Los Angeles, where he opens a nightclub and forms a connection with a homicide detective.</p>
+               
+                <div className="netflix-film-container">
+                    <img src={netflixLogo} alt="netflix" className="netflix-logo-banner"/>
+                    <p>FILM</p>
+                </div>
+                <img src={movieTitle} alt="movie title" className="movie-title"/>
+                <p className="top-ten-text-container">
+                    <span className="top-ten-text-span">TOP 10</span>
+                    <span className="rank-today-text">No. 5 in Movies Today</span>
+                </p>
+                <p className="banner-movie-description">A determined boy accepts a king's challenge to perform three impossible tasks in exchange for a magical elephant - and the chance to chase his destiny.</p>
                 <div className="banner-movie-button-container">
                     <button className="banner--play-button">
                         <span><BsFillPlayFill style={{fontSize: '1.7rem'}}/></span>
