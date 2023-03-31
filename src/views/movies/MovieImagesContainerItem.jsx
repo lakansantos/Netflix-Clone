2ed50be420522326590238d77606e3dbf7c2da65
ITW <rizesuoracle25@gmail.com>
2023-03-31 08:03:36 +0800

    fixed overflow

--- src/views/movies/MovieImagesContainerItem.jsx
+++ src/views/movies/MovieImagesContainerItem.jsx
@@ -34,66 +34,67 @@ const MovieImagesContainerItem = (props) => {
     }
     return (
         <div key={movie.id} 
-            className={imageWrapperClasses}  
-            onMouseEnter={() => handleHover(movie.id)}
-            onMouseLeave={() => handleLeaveHover()}>
-                {hasNumber ? 
-                <span className="movie-rank-text">{movie.id}</span> 
-                : 
-                null
-                }
-                <img 
-                src={isPoster ? movie.posterImage : movie.backgroundImage} 
-                alt={movie.title}
-                className={`${imageClasses} ${hovered ? 'disappear' : ''} `}
-                />
-                {hovered === movie.id &&
-                    <div className="image-show-hovered-container">
-                        <div className="movie-background" 
-                        style={{
-                            background: `url(${movie.backgroundImage}) 
-                            no-repeat 
-                            center/cover`,
-                        }}>
+        className={imageWrapperClasses}  
+        onMouseEnter={() => handleHover(movie.id)}
+        onMouseLeave={() => handleLeaveHover()}>
+            {hasNumber ? 
+            <span className="movie-rank-text">{movie.id}</span> 
+            : 
+            null
+            }
+            <img 
+            src={isPoster ? movie.posterImage : movie.backgroundImage} 
+            alt={movie.title}
+            className={`${imageClasses} ${hovered ? 'disappear' : ''} `
+            }
+            />
+            {hovered === movie.id &&
+                <div className="image-show-hovered-container">
+                    <div className="movie-background" 
+                    style={{
+                        background: `url(${movie.backgroundImage}) 
+                        no-repeat 
+                        center/cover`,
+                    }}>
+                    </div>
+                    <div className="movie-contents-wrapper">
+                        <div className="hovered-image-button-container">
+                            <div className="hovered-images-button-container-left">
+                                <button>
+                                <img src={playButton} alt="play button" />
+                                </button>
+                                <button>
+                                    <img src={check} alt="check button" />
+                                </button>
+                                <button>
+                                    <img src={like} alt="like button" />
+                                </button>
+                            </div>
+                            <div className="hovered-images-button-container-right">
+                                <button>
+                                    <img src={dropDown} alt="dropDown button" />
+                                </button>
+                            </div>
                         </div>
-                        <div className="movie-contents-wrapper">
-                            <div className="hovered-image-button-container">
-                                <div className="hovered-images-button-container-left">
-                                    <button>
-                                    <img src={playButton} alt="play button" />
-                                    </button>
-                                    <button>
-                                        <img src={check} alt="check button" />
-                                    </button>
-                                    <button>
-                                        <img src={like} alt="like button" />
-                                    </button>
-                                </div>
-                                <div className="hovered-images-button-container-right">
-                                    <button>
-                                        <img src={dropDown} alt="dropDown button" />
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="movie-hovered-texts">
-                                <p className="age-limit-text"> MA 15+</p>
-                                <p className="episode-text">16 episodes</p>
-                                <p className="resolution-text">HD</p>
-                            </div>
+                        <div className="movie-hovered-texts">
+                            <p className="age-limit-text"> MA 15+</p>
+                            <p className="episode-text">16 episodes</p>
+                            <p className="resolution-text">HD</p>
+                        </div>
 
-                            <div className="genres-text-container">
-                                <ul>
-                                    {movie.genres.map((genre, index)=> {
-                                        return (
-                                            <li key={index}>{genre}</li>
-                                        )
-                                    })}
-                                </ul>
-                            </div>
+                        <div className="genres-text-container">
+                            <ul>
+                                {movie.genres.map((genre, index)=> {
+                                    return (
+                                        <li key={index}>{genre}</li>
+                                    )
+                                })}
+                            </ul>
                         </div>
                     </div>
-                }
-            </div>
+                </div>
+            }
+        </div>
     )
 }
 
